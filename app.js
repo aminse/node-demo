c8d147867e857e5ecbd605d20b831e979a911775
aminse <aminse@gmail.com>
2022-02-21 12:02:49 +1100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Azure!!!');
+  res.send('Hello Amin!!! Welcome to Azure!');
 });
 
 app.listen(config.port, () => {
